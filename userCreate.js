f637e1ceb0de7f37f4d6c7feb89d48a52cc15aee
Albert Ngo <albertngo95@gmail.com>
2020-04-27 12:46:48 -0400

    creates user and checks for username duplicate

--- userCreate.js
+++ userCreate.js
@@ -20,13 +20,14 @@ let fetchServerData = function(serverStatus){
     }) //end of promise
 }
 
-let retrievedData;
 let serverData = fetchServerData(true, [1,2,3,4]);
 serverData.then((data)=> {
-    retrievedData = data;
-    console.log(retrievedData);})
+    // console.log(data);
+})
 
-//create new users
+
+
+//create new users================================================//
 class newUser{
     constructor(username, first, last, age){
         this.username = username;
@@ -38,6 +39,65 @@ class newUser{
     checkuserName(userID){}
     checkFirstLast(firstlast){}
     checkage(num){}
+}
+// ===============================================================//
+function checkUsername(name, array){
+    for (let user of array){
+        if (name == user.username)
+            return true;
+    }
+}
+
+
+function errorCheck (newUsers){
+    let accepted = [{username:"engeoh"}];
+    let rejected = [];
+    for (let user of newUsers){
+        let status = true;
+
+        try{ //check existing username
+            if (checkUsername(user.username,accepted) == true){
+                status = false;
+                rejected.push(user);
+                throw new Error (`Username: '${user.username}' already exists`)
+            }
+            }catch(error){
+                console.log(error.message);
+            }
+        //checkusername format
+        try{
+            
+        }
+        catch(error){
+                
+        }
+            //check first/last name
+
+                //check age > 21
+
+        if (status == true) accepted.push(user);
+
+    }
+     
+    return [accepted, rejected];
 
 }
+//==================================================================//
+let newUserArr = [];
+let acceptedUsers = []
+let unAcceptedUsers = [];
+
+newUserArr.push(new newUser("engeoh", "Alvin", "Ngo", 23));
+newUserArr.push(new newUser("engeoh2", "Bert", "Lee", 23));
+newUserArr.push(new newUser("engeoh", "Nall", "Singh", 23));
+
+let splitArray = errorCheck(newUserArr);
+[acceptedUsers, unAcceptedUsers] = splitArray;
+console.log("The accepted users are:");
+console.log(acceptedUsers);
+console.log("The rejected users are:");
+console.log(unAcceptedUsers);
+
+
+
 
